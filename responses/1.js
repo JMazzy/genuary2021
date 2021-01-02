5c7e3d335e78333b85704f64ec81cee72bedb402
Joshua Masland-Sarani <j.masland@gmail.com>
2021-01-01 23:53:53 -0600

    make a bit more dynamic

--- responses/1.js
+++ responses/1.js
@@ -4,26 +4,28 @@
 const width = 800;
 const height = 600;
 
+var colorOffset = 0;
+
 const s = p => {
     p.setup = function() {
       p.createCanvas(width, height);
+    };
+  
+    p.draw = function() {
       p.background(0);
       p.stroke(100);
         
       for (var i = 0; i < width; i+=10){
           for (var j = 0; j < height; j+=10){
               for (var k = 0; k < 360; k+=15){
+                  colorOffset+=1;
                 p.rotate(k);
-                let color = p.color(i % 255, j % 255, k % 255);
+                let color = p.color((i + colorOffset) % 255, (j + colorOffset) % 255, (k + colorOffset) % 255);
                 p.fill(color);
                 p.rect(i, j, k, k);
               }
           }
       }
-    };
-  
-    p.draw = function() {
-      
     };
   };
 
