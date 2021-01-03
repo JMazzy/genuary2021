0bbbe176ee9c3f4df942a00d4fe51427f6acbf83
Joshua Masland-Sarani <j.masland@gmail.com>
2021-01-03 00:44:07 -0600

    clean up some global state

--- responses/1.js
+++ responses/1.js
@@ -1,32 +1,31 @@
-// Prompt:
-// TRIPLE NESTED LOOP
-
-const width = 800;
-const height = 600;
-
-var colorOffset = 0;
+// January 1, 2021
+// Prompt: TRIPLE NESTED LOOP
 
 const s = p => {
-    p.setup = function() {
-      p.createCanvas(width, height);
-    };
+  const width = 800;
+  const height = 600;
+  var colorOffset = 0;
   
-    p.draw = function() {
-      p.background(0);
-      p.stroke(100);
-        
-      for (var i = 0; i < width; i+=10){
-          for (var j = 0; j < height; j+=10){
-              for (var k = 0; k < 360; k+=15){
-                  colorOffset+=1;
-                p.rotate(k);
-                let color = p.color((i + colorOffset) % 255, (j + colorOffset) % 255, (k + colorOffset) % 255);
-                p.fill(color);
-                p.rect(i, j, k, k);
-              }
-          }
-      }
-    };
+  p.setup = function () {
+    p.createCanvas(width, height);
   };
+
+  p.draw = function () {
+    p.background(0);
+    p.stroke(100);
+
+    for (var i = 0; i < width; i += 10) {
+      for (var j = 0; j < height; j += 10) {
+        for (var k = 0; k < 360; k += 15) {
+          colorOffset += 1;
+          p.rotate(k);
+          let color = p.color((i + colorOffset) % 255, (j + colorOffset) % 255, (k + colorOffset) % 255);
+          p.fill(color);
+          p.rect(i, j, k, k);
+        }
+      }
+    }
+  };
+};
 
 new p5(s, "p5-container");
--- responses/2.js
+++ responses/2.js
@@ -1,58 +1,60 @@
+// January 2, 2021
 // Prompt: Rule 30
 
-/* 
-current pattern	            111	110	101	100	011	010	001	000
-new state for center cell	 0 	 0	 0	 1	 1	 1	 1	 0
-*/
-
-const width = 800;
-const height = width/2;
-const center = Math.round(width / 2);
-var startingState = [];
-for (var i = 0; i < width; i++) {
-    startingState[i] = i === center ? 1 : 0;
-}
-var states = [startingState];
-var current = states[states.length - 1];
-
-function evaluateRule(pattern) {
-    switch (pattern) {
-        case "111":
-        case "110":
-        case "101":
-        case "000":
-            return 0;
-        case "100":
-        case "011":
-        case "010":
-        case "001":
-            return 1;
-        default:
-            return 0;
-    }
-}
-
-function getNextState(current) {
-    var next = [];
-    for (var i = 0; i < width - 2; i++) {
-        let pattern = "" + current[i] + current[i + 1] + current[i + 2];
-        next[i + 1] = evaluateRule(pattern);
-    }
-    return next;
-}
-
 const s = p => {
+    const evaluateRule = function(pattern) {
+        /* 
+        Wolfram Elementary Cellular Automata Rule 30
+        current pattern	            111	110	101	100	011	010	001	000
+        new state for center cell	 0 	 0	 0	 1	 1	 1	 1	 0
+        */
+    
+        switch (pattern) {
+            case "111":
+            case "110":
+            case "101":
+            case "000":
+                return 0;
+            case "100":
+            case "011":
+            case "010":
+            case "001":
+                return 1;
+            default:
+                return 0;
+        }
+    }
+    
+    const getNextState = function(current, width) {
+        var next = [];
+        for (var i = 0; i < width - 2; i++) {
+            let pattern = "" + current[i] + current[i + 1] + current[i + 2];
+            next[i + 1] = evaluateRule(pattern);
+        }
+        return next;
+    }
+
+    const width = 800;
+    const height = width/2;
+    var states = [];
+
     p.setup = function () {
         p.createCanvas(width, height);
         p.noStroke();
         p.background(0);
+
+        var startingState = [];
+        for (var i = 0; i < width; i++) {
+            startingState[i] = i === Math.round(width / 2) ? 1 : 0;
+        }
+        states.push(startingState);
     };
 
     p.draw = function () {
         var currentIdx = states.length - 1;
 
         if (states.length < height) {
-            states.push(getNextState(states[currentIdx]));
+            states.push(getNextState(states[currentIdx], width));
         }
 
         for (var i = 0; i < width; i++) {
